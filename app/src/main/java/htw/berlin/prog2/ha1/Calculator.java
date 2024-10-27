4e0c265f4c512a11c9865b47663a391f21cd8cf4
NurzhanSeitzhanov <Nurzhan.Seitzhanov@Student.HTW-Berlin.de>
2024-10-27 17:34:52 +0100

    Commit für Fix zu diesem Test

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -121,14 +121,19 @@ public class Calculator {
      * und das Ergebnis direkt angezeigt.
      */
     public void pressEqualsKey() {
-        var result = switch(latestOperation) {
-            case "+" -> latestValue + Double.parseDouble(screen);
-            case "-" -> latestValue - Double.parseDouble(screen);
-            case "x" -> latestValue * Double.parseDouble(screen);
-            case "/" -> latestValue / Double.parseDouble(screen);
-            default -> throw new IllegalArgumentException();
-        };
-        screen = Double.toString(result);
+
+        double currentValue = Double.parseDouble(screen);
+
+        if (latestOperation != null) {
+            latestValue = switch (latestOperation) {
+                case "+" -> latestValue + currentValue;
+                case "-" -> latestValue - currentValue;
+                case "x" -> latestValue * currentValue;
+                case "/" -> latestValue / currentValue;
+                default -> throw new IllegalArgumentException();
+            };
+        }
+        screen = Double.toString(latestValue);
         if(screen.equals("Infinity")) screen = "Error";
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
